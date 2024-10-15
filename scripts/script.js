43fe29b0a2c641125b1f91fa8f373e6afdf775ed
bsdc-isa <30274059@bsdc.ac.uk>
2024-10-15 21:54:32 +0100

    finished editing index

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,8 @@
-const about=document.getElementById("about");
-const services = document.getElementById("services");
 const insta = document.getElementById("insta");
 const snap = document.getElementById("snap");
 const whatsapp = document.getElementById("whatsapp");
 const x = document.getElementById("x");
 
-about.addEventListener("click", function(){
-    window.location.href="about.html";
-});
-services.addEventListener("click", function(){
-    window.location.href="services.html";
-})
 insta.addEventListener("click",function(){
     window.location.href="https://www.instagram.com/isaz.143?igsh=MWVsdTF3YWE1emRqbg==";
 });
